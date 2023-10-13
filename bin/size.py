a4b51e0048b183c3bb210f228ca2a268ec685e14
h-akira <79441679+h-akira@users.noreply.github.com>
2023-10-13 23:59:01 +0900

    add new option

--- bin/size.py
+++ bin/size.py
@@ -14,6 +14,7 @@ Calculate file and directory sizes.
   parser.add_argument("--version", action="version", version='%(prog)s 0.0.1')
   parser.add_argument("-l", "--symlinks", action="store_true", help="including symbolic links")
   parser.add_argument("-a", "--all", action="store_true", help="including hidden files and directories")
+  parser.add_argument("-s", "--itself", action="store_true", help="the specified thing itself")
   parser.add_argument("-d", "--max-display", metavar="number", type=int, default=80, help="maximum file or directory name length to display")
   parser.add_argument("path", metavar="input-file", help="input file")
   options = parser.parse_args()
@@ -43,20 +44,30 @@ def get_dir_size(path,symlinks=False):
 
 def main():
   options = parse_args()
-  file_list = os.listdir(options.path)
-  length_list = [len(s) for s in file_list]
-  max_length = min(max(length_list), options.max_display)
-  adjustment = [" "*(max(max_length-length,0)) for length in length_list]
-  for i,a in zip(file_list,adjustment):
-    if not options.all and i[0]==".":
-      continue
-    path = os.path.join(options.path, i)
-    if os.path.isfile(path):
-      size = os.path.getsize(path)
-      print(f"{i}{a}\t: {set_unit(size)}")
-    if os.path.isdir(path):
-      size = get_dir_size(path,options.symlinks)
-      print(f"{i}{a}\t: {set_unit(size)}")
+  if options.itself:
+    if os.path.isfile(options.path):
+      size = os.path.getsize(options.path)
+      print(f"{options.path}\t: {set_unit(size)}")
+    elif os.path.isdir(options.path):
+      size = get_dir_size(options.path,options.symlinks)
+      print(f"{options.path}\t: {set_unit(size)}")
+    else:
+      raise Exception
+  else:
+    file_list = os.listdir(options.path)
+    length_list = [len(s) for s in file_list]
+    max_length = min(max(length_list), options.max_display)
+    adjustment = [" "*(max(max_length-length,0)) for length in length_list]
+    for i,a in zip(file_list,adjustment):
+      if not options.all and i[0]==".":
+        continue
+      path = os.path.join(options.path, i)
+      if os.path.isfile(path):
+        size = os.path.getsize(path)
+        print(f"{i}{a}\t: {set_unit(size)}")
+      if os.path.isdir(path):
+        size = get_dir_size(path,options.symlinks)
+        print(f"{i}{a}\t: {set_unit(size)}")
 
 if __name__ == '__main__':
   main()
