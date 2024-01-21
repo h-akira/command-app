bd40300cbec552b9623812dcd4ea5b884fcadc79
h-akira <79441679+h-akira@users.noreply.github.com>
2024-01-22 01:07:31 +0900

    add new test file

---
+++ bin/speed_test2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Created: 2024-01-21 13:44:34
+
+def parse_args():
+  import argparse
+  # from python import argparse
+  parser = argparse.ArgumentParser(description="""\
+
+""", formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument("--version", action="version", version='%(prog)s 0.0.1')
+  parser.add_argument("-i", "--iter", metavar="iter", type=int, default=10000, help="iter")
+  parser.add_argument("-s", "--size", metavar="size", type=int, default=10000, help="size")
+  # parser.add_argument("-", "--", action="store_true", help="")
+  # parser.add_argument("file", metavar="input-file", help="input file")
+  options = parser.parse_args()
+  return options
+
+def main():
+  # from python import time
+  import time
+  options = parse_args()
+  xs = list(range(options.size))
+  start = time.time()
+  for i in range(options.iter):
+    if i%(options.iter//1000) == 0:
+      print("\r"+f"{i/options.iter*100:1f}/100", end="")
+    for j in range(len(xs)-1):
+      xs[j] = xs[j]+xs[j+1]
+    xs[-1] = xs[-1]+xs[0]
+    for j in range(len(xs)):
+      if xs[j] > 10000:
+        xs[j] = xs[j]%27
+  print("\r"+"100/100",end="")
+  print()
+  end = time.time()
+  print("time: ", end-start)
+
+if __name__ == '__main__':
+  main()
